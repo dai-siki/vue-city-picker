9121f951c2c8130c87b764b72379509404ed1f22
dai-siki <851733175@qq.com>
2016-11-16 13:54:19 +0800

    新鲜出炉

---
+++ README.md
@@ -0,0 +1,92 @@
+# vue-city-picker
+
+vue城市选择组件（按城市首字母排序）
+
+
+## 更新日志
+
+### @1.0.0
+- 我出生了
+
+
+## demo
+
+示例（vue@1.0+） [点我](http://dai-siki.github.io/vue-city-picker/example/demo.html).
+
+
+## 截图
+
+![screenshot](screenshot/01.png)
+![screenshot](screenshot/02.png)
+
+
+## 依赖
+
+基于 vue.js@1.0+ 版本，模块使用es6编写（需babel转译），css使用scss编译（需scss-loader转译）。
+
+
+## 安装
+
+#### npm
+
+```shell
+$ npm install vue-city-picker
+```
+
+## 使用
+
+参考示例，城市列表需要按指定的格式，并声明全局变量。
+
+#### 相关参数
+
+| 名称              | 类型               | 默认             | 说明                                         |
+| ----------------- | ----------------- | ---------------- | --------------------------------------------- |
+| value             | String            | ""               | 要双向绑定的城市Id                |
+| field             | String            | ""               | 会给input标签添加name及id属性                   |
+| placeholder       | String            | ""               | 你懂的                                     |
+
+
+#### 示例(vue@1.0+)
+
+```html
+
+<div id="app">
+	<label for="city">选择购车城市</label>
+	<city-picker field="city" placeholder="选择日期" :value.sync="cityId"></city-picker>
+</div>
+
+<script>
+/**
+ * 引入城市列表，格式：
+ * {
+ * 		//热门
+ * 		hot:[
+ * 			{id:1, name:'北京'}
+ * 		],
+ * 		A:[
+ * 			{id:2, name:'澳门'}
+ * 		]
+ * 	}
+ */
+import cityList from 'vue-city-picker/json/city-picker-list.json';
+//因为使用了es6的一些方法，需要babel垫片，如果你项目中已有相关兼容性方案，可忽略
+import 'babel-polyfill';
+import Vue from 'vue';
+import myCityPicker from 'vue-city-picker';
+
+//注册一个城市列表全局变量（考虑到cityList会比较大，单独为一个js文件比较好）
+window.CITY_PICKER_LIST = cityList;
+
+new Vue({
+	el: '#app',
+	data: {
+		cityId: ''
+	},
+	components: {
+		'city-picker': myCityPicker
+	}
+});
+
+</script>
+
+```
---
+++ city-picker.vue
@@ -0,0 +1,130 @@
+<template>
+	<div class="vue-city-picker" @mouseout="endChoice" @mouseover="startMouseOver">
+		<input type="text" :name="field" :id="field" :value="currCityName" :placeholder="placeholder" @click="startChoice"
+		@keypress="startChoice">
+		<!--城市选择-->
+		<div class="vcp-panel" v-show="cityPanelIsShow">
+			<h5>城市选择</h5>
+			<ul class="vcp-tags">
+				<li :class="{'z-on': item==tagKey}" v-for="item in tagsArr" @click="choiceTag(item)">{{ item }}</li>
+			</ul>
+			<div class="vcp-list">
+				<div v-for="(initial, citys) in cityList" v-show="currTagIncludes(initial)">
+					<h6 v-if="initial != 'hot'">{{ initial }}</h6>
+					<ul>
+						<li v-for="v in citys" @click="choiceCity(v.id)" :title="v.name">{{ v.name }}</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+'use strict';
+
+export default {
+	props: {
+		field: {
+			type: String,
+			default: ''
+		},
+		value: {
+			default: '',
+			twoWay: true
+		},
+		placeholder: {
+			type: String,
+			default: ''
+		}
+	},
+	data() {
+		let {
+			value
+		} = this,
+		cityList = window.CITY_PICKER_LIST,
+		tagsArr = ['热门', 'ABCDE', 'FGHJ', 'KLMN', 'PQRST', 'WXYZ'],
+		tags = {};
+		tagsArr.forEach((item)=>{
+			if(item == '热门'){
+				tags[item] = ['hot'];
+			}else{
+				tags[item] = item.split('');
+			}
+		});
+		return {
+			cityPanelIsShow: false,
+			isMouseOver: false,
+			cityList,
+			tagsArr,
+			tags,
+			tagKey: '热门'
+		};
+	},
+	computed: {
+		// 当前城市名字
+		currCityName() {
+			let {
+				value,
+				cityList
+			} = this,
+			cityName = '';
+			Object.keys(cityList).forEach((item)=>{
+				cityList[item].forEach((item)=>{
+					if(value == item.id){
+						cityName = item.name;
+					}
+				})
+			});
+			return cityName;
+		},
+		currTag(){
+			let {tagKey, tags} = this;
+			return tags[tagKey];
+		}
+	},
+	methods: {
+		// 列表是否属于当前标签
+		currTagIncludes(ini){
+			let {currTag} = this;
+			return currTag.includes(ini);
+		},
+		// 选择标签
+		choiceTag(key){
+			this.tagKey = key;
+		},
+		// 开始城市选择
+		startChoice(e) {
+			if (e && e.type == 'keypress') {
+				e.returnValue = false;
+			}
+			this.cityPanelIsShow = true;
+		},
+		choiceCity(id) {
+			this.value = id;
+			this.immEndChoice();
+		},
+		// 鼠标离开城市选择区域时超过一定时间，关闭城市面板
+		endChoice() {
+			let that = this;
+			that.isMouseOver = false;
+			setTimeout(function() {
+				if (!that.isMouseOver) {
+					that.cityPanelIsShow = false;
+				}
+			}, 1000);
+		},
+		startMouseOver() {
+			this.isMouseOver = true;
+		},
+		// 立即关闭城市面板
+		immEndChoice() {
+			this.isMouseOver = true;
+			this.cityPanelIsShow = false;
+		}
+	}
+}
+
+</script>
+
+<style lang="sass" scoped>@import "./scss/city-picker.scss"</style>
